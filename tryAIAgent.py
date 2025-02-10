b5ef6c1afda58a663afccb733589399f16fd23ac
973040629@qq.com <973040629@qq.com>
2025-02-10 16:57:33 +0800

    add

--- tryAIAgent.py
+++ tryAIAgent.py
@@ -2,6 +2,15 @@ from langchain_community.document_loaders import PDFPlumberLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain.prompts import ChatPromptTemplate
+#from langchain_ollama import OllamaLLM
+from langchain_openai import ChatOpenAI
+from langchain.chains import RetrievalQA
+#from langchain_core.runnables import RunnablePassthrough
+#from langchain import hub
+#import os
+#os.environ["LANGCHAIN_TRACING_V2"] = "true"
+#os.environ["LANGCHAIN_API_KEY"] = ""
 
 def doc_loader():
     """
@@ -33,7 +42,7 @@ def embedding_docs(chunks):
         FAISS: 存储了文档嵌入的向量数据库。
     """
     # 初始化文档嵌入，并使用指定的模型创建向量数据库
-    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    embeddings = OllamaEmbeddings(model="nomic-embed-text:latest")
     vector_db = FAISS.from_documents(chunks, embeddings)
     
     # 将向量数据库保存到本地
@@ -49,20 +58,55 @@ def loading_vector_db():
         FAISS: 从本地加载的向量数据库。
     """
     # 从指定路径加载向量数据库
-    vector_db = FAISS.load_local("faiss_index", OllamaEmbeddings(model="nomic-embed-text"), allow_dangerous_deserialization=True)
+    vector_db = FAISS.load_local("faiss_index", OllamaEmbeddings(model="nomic-embed-text:latest"), allow_dangerous_deserialization=True)
     
     return vector_db
+
+
 
 if __name__ == "__main__":
     print("Starting...\n")
     
     # 加载并分割文档
-    chunks = doc_loader()
-    print("切片完成\n")
+    #chunks = doc_loader()
+    #print("切片完成\n")
     
     # 加载向量数据库
     vector_db = loading_vector_db()
 
-    # 执行相似度搜索并打印结果
-    docs = vector_db.similarity_search("W630")
-    print(docs[0].page_content)
+    llm = ChatOpenAI(api_key="",
+                 model="deepseek-ai/DeepSeek-R1-Distill-Qwen-7B",
+                 base_url="https://api.siliconflow.cn/v1")
+    
+    prompt = """
+    请严格根据以下上下文回答问题,力求答案简洁：
+    {context}
+
+    问题：{question}
+    如果上下文不相关，请回答“我不确定，请联系客服”。
+    答案：
+    """
+  
+    rag_prompt = ChatPromptTemplate([("system",prompt)])
+    retriever = vector_db.as_retriever()
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        chain_type_kwargs={"prompt": rag_prompt},
+        return_source_documents=True
+        )
+    question = "如何开启wlan功能？"
+    result = qa_chain.invoke({"query": question})
+    print(result["result"])
+
+
+
+   
+
+
+
+
+    
+    
+    
